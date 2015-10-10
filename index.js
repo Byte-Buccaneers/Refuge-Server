f7e7463c864fa0647540980f9193b0d9ea808e49
Cassidy Schaufele <cschaufe@kent.edu>
2015-10-10 17:09:51 -0400

    I don't know what the fuck is going on

--- index.js
+++ index.js
@@ -5,12 +5,33 @@ var express = require('express'),
     server = require('http').Server(app),
     stdio = require('stdio'),
     asyncd = require('async'),
-	mongo = require('mongodb'),
-	mongojs = require('mongojs');
+	  MongoClient = require('mongodb').MongoClient,
+    assert = require('assert');
 
-var mongoUri = 'mongodb://admin:admin@ds035674.mongolab.com:35674/byte-buccaneers'
-var db = mongojs(mongoUri,['users','groups']);
+var mongoUri = 'mongodb://b:b@ds041633.mongolab.com:41633/second'
 
+// Connection URL
+var url = 'mongodb://b:b@ds035674.mongolab.com:35674/byte-buccaneers';
+// Use connect method to connect to the Server
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server");
+  findDocuments(db,function(d){
+    console.log(d);
+  });
+});
+
+
+var findDocuments = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+  // Find some documents
+  collection.find({}).toArray(function(err, docs) {
+    console.log("Found the following records");
+    console.dir(docs);
+    callback(docs);
+  });
+}
 
 
 //App Configuration
@@ -21,65 +42,65 @@ app.get('/', function(request, response) {
   response.render('index.html');
 });
 
-
-app.get('/create', function(request, response){
-  q = request.query;
-  db.users.findAndModify({
-    query:{userID: q.userID,
-           website: q.website},
-    update:{
-    $setOnInsert: {user: q.user,
-                   password: q.password,
-                   website: q.website,
-                   userID: q.userID}
-    },
-    new: true,
-    upsert: true // insert the document if it does not exist
-  },function (err, doc, lastErrorObject) {
-    // doc.tag === 'maintainer'
-  })
-
-  response.json(request.query);
-})
-
-app.get('/read/:number/:name/', function(request, response){
-	//request.params.name/number/whatever
-	var usernumber = request.params.number;
-	db.users.find({},function(err, docs) {
-		console.log(err);
-	});
-	/*
-	var labs_res =
-	db.users.find(
-		{"number":usernumber},
-		function(err,docs){
-			console.log(docs);
-			response.send(docs);
-		}
-	);
-	*/
-	/*
-	db.users.find({phonenumber: usernumber},function(err, docs){
-     console.log(docs);
-     response.json(docs);
-    })*/
-	response.send("shutup \n")
-})
-
-app.get('/update', function(request,response){
-  q = request.query;
-  db.users.update({userID : q.userID, website: q.website},
-                  {$set: {password: q.password}},
-                  function(err, docs){
-    response.json(docs);
-  })
-})
-
-
-app.get('/api/test', function(request, response){
-    var q = request.query;
-    console.log(q);
-});
+//
+// app.get('/create', function(request, response){
+//   q = request.query;
+//   db.users.findAndModify({
+//     query:{userID: q.userID,
+//            website: q.website},
+//     update:{
+//     $setOnInsert: {user: q.user,
+//                    password: q.password,
+//                    website: q.website,
+//                    userID: q.userID}
+//     },
+//     new: true,
+//     upsert: true // insert the document if it does not exist
+//   },function (err, doc, lastErrorObject) {
+//     // doc.tag === 'maintainer'
+//   })
+//
+//   response.json(request.query);
+// })
+//
+// app.get('/read/:number/:name/', function(request, response){
+// 	//request.params.name/number/whatever
+// 	var usernumber = request.params.number;
+// 	db.users.find({},function(err, docs) {
+// 		console.log(err);
+// 		console.log(docs);
+// 	});
+// 	/*
+// 	var labs_res =
+// 	db.users.find(
+// 		{"number":usernumber},
+// 		function(err,docs){
+// 			console.log(docs);
+// 			response.send(docs);
+// 		}
+// 	);
+// 	*/
+// 	/*
+// 	db.users.find({phonenumber: usernumber},function(err, docs){
+//      console.log(docs);
+//      response.json(docs);
+//     })*/
+// })
+//
+// app.get('/update', function(request,response){
+//   q = request.query;
+//   db.users.update({userID : q.userID, website: q.website},
+//                   {$set: {password: q.password}},
+//                   function(err, docs){
+//     response.json(docs);
+//   })
+// })
+//
+//
+// app.get('/api/test', function(request, response){
+//     var q = request.query;
+//     console.log(q);
+// });
 
 
 server.listen(app.get('port'), function() {
